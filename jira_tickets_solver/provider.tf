169304fba46c3c012fa5737a117515aa996fda76
Constantin Paigin <koss1103@gmail.com>
2024-04-25 18:20:53 +0300

    ARM_ACCESS_KEY

--- jira_tickets_solver/provider.tf
+++ jira_tickets_solver/provider.tf
@@ -3,13 +3,7 @@ terraform {
     storage_account_name = "tfstate25604"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    access_key           = var.access_key
   }
-}
-
-variable "access_key" {
-  type        = string
-  description = "Access key for storage account"
 }
 
 provider "azurerm" {
