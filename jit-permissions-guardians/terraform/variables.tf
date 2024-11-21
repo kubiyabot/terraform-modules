da98bf4f0b61c955404bb05e7b47ce82a2d1974d
koss110 <costa@kubiya.ai>
2024-11-21 17:16:53 +0200

    kubernetes integration removed

--- jit-permissions-guardians/terraform/variables.tf
+++ jit-permissions-guardians/terraform/variables.tf
@@ -33,7 +33,7 @@ variable "request_tools_sources" {
 variable "kubiya_integrations" {
   description = "List of Kubiya integrations to enable. Supports multiple values. \n For AWS integration, the main account must be provided."
   type        = list(string)
-  default     = ["slack", "kubernetes"]
+  default     = ["slack"]
 }
 
 variable "kubiya_tool_timeout" {
