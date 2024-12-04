c25c63bacefc35d37374ef50bfbee99c40efd70a
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-12-04 07:15:20 +0200

    fixed main tf

--- terraform_modules_self_service_kiosk/terraform/variables.tf
+++ terraform_modules_self_service_kiosk/terraform/variables.tf
@@ -27,21 +27,7 @@ variable "kubiya_groups_allowed_groups" {
 variable "tf_module_config_yaml" {
   description = "Terraform module configuration in YAML format."
   type        = string
-  default     = <<-EOT
-    aws_sqs:
-      name: "AWS SQS"
-      description: "Creates an SQS queue with all configurations"
-      source:
-        location: "https://github.com/terraform-aws-modules/terraform-aws-sqs"
-        version: "master"
-
-    aws_s3:
-      name: "AWS S3" 
-      description: "Creates an S3 bucket with optional configurations"
-      source:
-        location: "https://github.com/terraform-aws-modules/terraform-aws-s3-bucket"
-        version: "master"
-  EOT
+  default     = "test"
 }
 
 # Secrets (Kubiya Secrets)
