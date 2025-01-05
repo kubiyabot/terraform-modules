dad917ec4cc83b8b810a686b26e215973c6ee90c
mevrat.avraham <mevrat.avraham@kubiya.ai>
2025-01-05 13:53:07 +0200

    json validate

--- jit-permissions-guardians/terraform/main.tf
+++ jit-permissions-guardians/terraform/main.tf
@@ -23,13 +23,13 @@ resource "kubiya_source" "jit_approval_workflow_tooling" {
     {
       "org": "${var.org_name}",
       "policy": "${var.opa_policy}",
-      "runner": "${var.kubiya_runner}",
-      "dd_site": "${var.dd_site}",
-      "dd_api_key": "${var.dd_api_key}",
-      "okta_base_url": "${var.okta_base_url}",
-      "okta_client_id": "${var.okta_client_id}",
-      "okta_private_key": "${var.okta_private_key}",
-      "okta_token_endpoint": "${var.okta_base_url}/oauth2/v1/token"
+      "runner": "${var.kubiya_runner}"${var.okta_enabled ? "," : ""}
+      ${var.okta_enabled ? "\"okta_base_url\": \"${var.okta_base_url}\"," : ""}
+      ${var.okta_enabled ? "\"okta_client_id\": \"${var.okta_client_id}\"," : ""}
+      ${var.okta_enabled ? "\"okta_private_key\": \"${var.okta_private_key}\"," : ""}
+      ${var.okta_enabled ? "\"okta_token_endpoint\": \"${var.okta_base_url}/oauth2/v1/token\"" : ""}${var.dd_enabled ? "," : ""}
+      ${var.dd_enabled ? "\"dd_site\": \"${var.dd_site}\"," : ""}
+      ${var.dd_enabled ? "\"dd_api_key\": \"${var.dd_api_key}\"" : ""}
     }
   EOT
 }
