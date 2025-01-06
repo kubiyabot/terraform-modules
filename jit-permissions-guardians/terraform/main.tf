a4ca3715fec660e7c84da87a159f3da4565423d1
mevrat.avraham <mevrat.avraham@kubiya.ai>
2025-01-06 15:16:20 +0200

    Update enforcer source URL and runner name configuration
    Replaced the enforcer source URL with the main branch URL for clarity and maintainability. Updated the runner name to use a variable for consistency and better dynamic configuration management.

--- jit-permissions-guardians/terraform/main.tf
+++ jit-permissions-guardians/terraform/main.tf
@@ -21,10 +21,10 @@ data "http" "jit_access_knowledge" {
 
 # Configure sources
 resource "kubiya_source" "enforcer_source" {
-  url            = "https://github.com/kubiyabot/community-tools/tree/CORE-748-setup-jit-usecase-with-the-enforcer-being-setup-automatically-with-memory-on-cloud-policy-pulled-dynamic-config-refactor-to-opal/just_in_time_access_proactive"
+  url = "https://github.com/kubiyabot/community-tools/tree/main/just_in_time_access_proactive"
   runner         = var.kubiya_runner
   dynamic_config = jsonencode({
-    opa_runner_name = "mevratm"
+    opa_runner_name = var.kubiya_runner
     opa_default_policy = data.http.opa_default_policy.response_body
   })
   depends_on = [data.http.opa_default_policy]
