9c3a3aaeac2f4cd531fbfb01a85e48c32295dcb9
mevrat.avraham <mevrat.avraham@kubiya.ai>
2025-01-19 19:08:43 +0200

    [Message] Update JIT enforcer source URL to main branch
    The source URL was changed to reference the main branch instead of a specific feature branch. This shift ensures continued access to the latest and stable code updates for the JIT enforcer.

--- jit-permissions-guardians/terraform/main.tf
+++ jit-permissions-guardians/terraform/main.tf
@@ -17,7 +17,7 @@ data "http" "jit_access_knowledge" {
 
 # Configure sources
 resource "kubiya_source" "enforcer_source" {
-  url    = "https://github.com/kubiyabot/community-tools/tree/CORE-748-setup-jit-usecase-with-the-enforcer-being-setup-automatically-with-memory-on-cloud-policy-pulled-dynamic-config-refactor-to-opal/just_in_time_access_proactive"
+  url    = "https://github.com/kubiyabot/community-tools/tree/main/just_in_time_access_proactive"
   runner = var.kubiya_runner
   dynamic_config = jsonencode({
     aws_jit_config      = var.config_json
